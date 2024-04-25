d580aa9113b75c38f479f4df6ba509e8cd930f54
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2024-04-25 17:28:32 +0200

    added emails to about us added email field to employee model copied employee model into backend

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -10,8 +10,10 @@
  *  oder response.end()
  */
 import { Request, Response } from 'express';
+import { ALL_EMPLOYEES } from './static-data/employees';
 
 export class ApiController {
+
   public getInfo(request: Request, response: Response): void {
     response.status(200);
     response.send('ok');
@@ -20,12 +22,6 @@ export class ApiController {
   // want all the information about all the employees
   public getEmployeeInfo(req: Request, resp: Response): void {
     resp.status(200);
-    resp.send([
-        { name: 'Igor Ziesmann', info: 'Ich studiere Informatik mit dem Schwerpunkt Data Science an der Uni Konstanz.', imgUrl: '/assets/about-us/igorz.jpg' },
-        { name: 'Jonas Dickhoefer', info: 'I study Informatics in the 4th semester, focusing on machine learning. In my free time, I like to do Karate.', imgUrl: '/assets/about-us/jonasd.png' },
-        { name: 'Max Oltmanns', info: 'I\'m studying computer science at the University of Konstanz, and I like to spend my free time in the mountains.', imgUrl: '/assets/about-us/maxo.png' },
-        { name: 'Maximilian Jaeger', info: 'I study Informatics and i love skiing in my spare time.', imgUrl: '/assets/about-us/maximilianj.png' },
-        { name: 'Silvan Ronge', info: 'Still believes that computer science is a creative art form.', imgUrl: '/assets/about-us/silvanr.png' }
-    ]);
-}
+    resp.send(ALL_EMPLOYEES);
+  }
 }
---
+++ backend/src/static-data/employees.ts
@@ -0,0 +1,9 @@
+import { Employee } from '../models/employee';
+
+export const ALL_EMPLOYEES: Employee[] = [
+    { name: 'Igor Ziesmann', info: 'Ich studiere Informatik mit dem Schwerpunkt Data Science an der Uni Konstanz.', imgUrl: '/assets/about-us/igorz.jpg', email: 'igor.ziesmann@uni-konstanz.de' },
+    { name: 'Jonas Dickhoefer', info: 'I study Informatics in the 4th semester, focusing on machine learning. In my free time, I like to do Karate.', imgUrl: '/assets/about-us/jonasd.png', email: 'jonas.dickohefer@uni-konstanz.de' },
+    { name: 'Max Oltmanns', info: 'I\'m studying computer science at the University of Konstanz, and I like to spend my free time in the mountains.', imgUrl: '/assets/about-us/maxo.png', email: 'max.oltmanns@uni-konstanz.de' },
+    { name: 'Maximilian Jaeger', info: 'I study Informatics and i love skiing in my spare time.', imgUrl: '/assets/about-us/maximilianj.png', email: 'maximilian.jaeger@uni-konstanz.de' },
+    { name: 'Silvan Ronge', info: 'Still believes that computer science is a creative art form.', imgUrl: '/assets/about-us/silvanr.png', email: 'silvan.ronge@uni-konstanz.de' }
+  ];
