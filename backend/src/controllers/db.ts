5f8a16a528fe36a9c226c0cad3ffb106843c37fe
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2024-04-26 18:51:44 +0200

    created users table model

--- backend/src/controllers/db.ts
+++ backend/src/controllers/db.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from 'express';
 import { Sequelize } from 'sequelize';
-import * as defaults from 'src/models/sequelize/user';
+//import * as defaults from 'src/models/sequelize/user';
 
 const sequelize = new Sequelize('postgres://admin:spro-g6-db@localhost:5432/postgres'); // connect to the database
 
-const usersTable = sequelize.define('Users', userAttribs);
+//const usersTable = sequelize.define('Users', userAttribs);
 
 export class DatabaseController {
     public status(req: Request, resp: Response): void {
-        // TODO make sure the database is running!
         sequelize.authenticate()
             .then(() => {
                 resp.status(200);
