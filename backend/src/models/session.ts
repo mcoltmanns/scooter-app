3c8d5ba0ac2f21b277463c04754f19d50c202247
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2024-04-29 10:41:25 +0200

    removed session key from auth table -> because auths can have many sessions

--- backend/src/models/session.ts
+++ backend/src/models/session.ts
@@ -18,13 +18,7 @@ export const UsersSession = Database.getSequelize().define('usersSession', {
     }
 }, { updatedAt: false, createdAt: false });
 
-UsersSession.hasOne(UsersAuth, { // sessions have exactly one user
-    foreignKey: {
-        name: 'usersSessionId',
-        allowNull: true, // users aren't always logged in
-    }
-});
-UsersAuth.hasMany(UsersSession, { // but users may have many simultaneous sessions
+UsersAuth.hasMany(UsersSession, { // users may have many simultaneous sessions
     foreignKey: {
         name: 'usersAuthId',
         allowNull: false, // every session must have a user
