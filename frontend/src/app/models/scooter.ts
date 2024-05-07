b363492ebde6630832b37bdf0280e865b394114c
Igor Ziesmann <igor.ziesmann@uni-konstanz.de>
2024-05-07 15:01:23 +0200

    edited models/scooter.ts

---
+++ frontend/src/app/models/scooter.ts
@@ -0,0 +1,8 @@
+// Data structure for scooter information.
+export type Scooter = {
+    id: number, 
+    product_id: string,
+    battery: number,
+    coordinates_lat: number,
+    coordinates_lng: number
+}
