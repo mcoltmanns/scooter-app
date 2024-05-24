65284299efa456501630f5da76e30686492462c2
jonas.dickhoefer <jonas.dickhoefer@uni-konstanz.de>
2024-05-24 19:17:33 +0200

    fixed back button

--- frontend/src/app/pages/payment/payment.component.ts
+++ frontend/src/app/pages/payment/payment.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { BackButtonComponent } from 'src/app/components/back-button/back-button.component';
 import { ButtonComponent } from 'src/app/components/button/button.component';
 import { UserInputComponent } from 'src/app/components/user-input/user-input.component';
@@ -14,4 +14,10 @@ import { PaymentOptionsComponent } from 'src/app/components/payment-options/paym
     styleUrl: './payment.component.css',
     imports: [UserInputComponent, ButtonComponent, RouterLink, BackButtonComponent, ReactiveFormsModule, PaymentOptionsComponent]
 })
-export class PaymentComponent {}
+export class PaymentComponent {
+    constructor(private router: Router) { }
+
+  back(): void {
+        this.router.navigate(['settings']);
+  }
+}
