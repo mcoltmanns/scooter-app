76fdbe8e83cd7b2e0e10a652f482b4d3ea636f70
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2024-05-29 19:28:36 +0200

    added hcipal interface

---
+++ backend/src/services/payment/hcipal.ts
@@ -0,0 +1,53 @@
+import post from 'axios';
+
+abstract class HciPal {
+    private static processResponse(data: string, fieldWanted: string): {status: number, message: string} {
+        const jsonData = JSON.parse(data);
+        if(jsonData.success) {
+            return {status: 200, message: fieldWanted === '' ? '' : jsonData[fieldWanted]};
+        }
+        return {status: 400, message: jsonData.error};
+    }
+
+    public static getCountryCode(accountName: string): Promise<{status: number, message: string}> {
+        const data = { accountName: accountName };
+        return new Promise((resolve) => {
+            post('https://pass.hci.uni-konstanz.de/hcipal/country', {data: data, method: 'POST'})
+            .then((response) => {
+                resolve(this.processResponse(JSON.stringify(response.data), 'country'));
+            });
+        });
+    }
+
+    public static initTransaction(accountName: string, accountPassword: string, amount: number): Promise<{status: number, message: string}> {
+        const data = { accountName: accountName, accountPassword: accountPassword, amount: amount };
+        return new Promise((resolve) => {
+            post('https://pass.hci.uni-konstanz.de/hcipal/check', {data: data, method: 'POST'})
+            .then((response) => {
+                resolve(this.processResponse(JSON.stringify(response.data), 'token'));
+            });
+        });
+    }
+
+    public static commitTransaction(token: string): Promise<{status: number, message: string}> {
+        const data = {token: token};
+        return new Promise((resolve) => {
+            post('https://pass.hci.uni-konstanz.de/hcipal/payment', {data: data, method: 'POST'})
+            .then((response) => {
+                resolve(this.processResponse(JSON.stringify(response.data), ''));
+            });
+        });
+    }
+
+    public static rollbackTransaction(token: string): Promise<{status: number, message: string}> {
+        const data = {token: token};
+        return new Promise((resolve) => {
+            post('https://pass.hci.uni-konstanz.de/hcipal/payback', {data: data, method: 'POST'})
+            .then((response) => {
+                resolve(this.processResponse(JSON.stringify(response.data), ''));
+            });
+        });
+    }
+}
+
+export default HciPal;
