a2a19a11846e74fe83758990e761f45d830d3b2d
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2024-05-29 12:20:24 +0200

    more swpsafe refactoring - abstracted response processing

--- backend/src/services/payment/swpsafe.ts
+++ backend/src/services/payment/swpsafe.ts
@@ -2,36 +2,35 @@ import { parse } from 'csv-parse';
 import get from 'axios';
 
 abstract class SwpSafe {
-    public static async getCountryCode(accountId: string): Promise<{ status: number; message: string; }> {
+    private static processResponse(data: string, fieldWanted: string): Promise<{ status: number, message: string }> {
         return new Promise((resolve) => {
-            get(`https://pass.hci.uni-konstanz.de/swpsafe/country/code/${encodeURIComponent(accountId)}`)
-                .then((response) => {
-                    parse(response.data, {delimiter: ',', skip_empty_lines: true}, (err, records) => {
-                        if(err) {
-                            resolve({ status: 500, message: 'could not parse csv' });
-                        }
-                        resolve({status: parseInt(records[1][0]), message: records[1][0] === '200' ? records[1][3] : records[1][1]});
-                    });
-                })
-                .catch((error) => {
-                    resolve({status: 500, message: `could not get country code: ${error}`});
-                });
+            parse(data, {delimiter: ',', skip_empty_lines: true}, (err, records) => {
+                if(err) resolve({status: 500, message: `csv parse error: ${err}`});
+                resolve({status: parseInt(records[1][0]), message: records[1][records[0].indexOf(records[1][0] === '200' ? fieldWanted : 'errormessage')]});
+            });
         });
     }
 
-    public static getTransaction(accountId: string, amount: number): Promise<{status: number, message: string}> {
+    public static getCountryCode(accountId: string): Promise<{ status: number; message: string; }> {
+        return new Promise((resolve) => {
+            get(`https://pass.hci.uni-konstanz.de/swpsafe/country/code/${encodeURIComponent(accountId)}`)
+            .then((response) => {
+                resolve(this.processResponse(response.data, 'country'));
+            })
+            .catch((error) => {
+                resolve(this.processResponse(error.response.data, 'errormessage'));
+            });
+        });
+    }
+
+    public static initTransaction(accountId: string, amount: number): Promise<{status: number, message: string}> {
         return new Promise((resolve) => {
             get(`https://pass.hci.uni-konstanz.de/swpsafe/check/code/${encodeURIComponent(accountId)}/amount/${encodeURIComponent(amount.toString())}`)
             .then((response) => {
-                parse(response.data, {delimiter: ',', skip_empty_lines: true}, (err, records) => {
-                    if(err) {
-                        resolve({ status: 500, message: 'could not parse csv' });
-                    }
-                    resolve({status: parseInt(records[1][0]), message: records[1][0] === '200' ? records[1][2] : records[1][1]});
-                });
+                resolve(this.processResponse(response.data, 'token'));
             })
             .catch((error) => {
-                resolve({status: 500, message: `could not get transaction: ${error}`});
+                resolve(this.processResponse(error.response.data, 'errormessage'));
             });
         });
     }
@@ -40,15 +39,10 @@ abstract class SwpSafe {
         return new Promise((resolve) => {
             get(`https://pass.hci.uni-konstanz.de/swpsafe/use/${encodeURIComponent(token)}`)
             .then((response) => {
-                parse(response.data, {delimiter: ',', skip_empty_lines: true}, (err, records) => {
-                    if(err) {
-                        resolve({ status: 500, message: 'could not parse csv' });
-                    }
-                    resolve({status: parseInt(records[1][0]), message: records[1][1]});
-                });
+                resolve(this.processResponse(response.data, 'errormessage'));
             })
             .catch((error) => {
-                resolve({status: 500, message: `could not commit transaction: ${error}`});
+                resolve(this.processResponse(error.response.data, 'errormessage'));
             });
         });
     }
@@ -57,15 +51,10 @@ abstract class SwpSafe {
         return new Promise((resolve) => {
             get(`https://pass.hci.uni-konstanz.de/swpsafe/use/${encodeURIComponent(token)}`)
             .then((response) => {
-                parse(response.data, {delimiter: ',', skip_empty_lines: true}, (err, records) => {
-                    if(err) {
-                        resolve({ status: 500, message: 'could not parse csv' });
-                    }
-                    resolve({status: parseInt(records[1][0]), message: records[1][1]});
-                });
+                resolve(this.processResponse(response.data, 'errormessage'));
             })
             .catch((error) => {
-                resolve({status: 500, message: `could not roll back transaction: ${error}`});
+                resolve(this.processResponse(error.response.data, 'errormessage'));
             });
         });
     }
