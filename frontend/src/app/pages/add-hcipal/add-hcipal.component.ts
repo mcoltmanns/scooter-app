9b7d55af8d0bcd496f41c47e5b94a187ebf16b60
Silvan Ronge <silvan.ronge@uni-konstanz.de>
2024-06-02 06:23:02 +0200

    Improve showing the toast after routing

--- frontend/src/app/pages/add-hcipal/add-hcipal.component.ts
+++ frontend/src/app/pages/add-hcipal/add-hcipal.component.ts
@@ -167,7 +167,7 @@ export class AddhcipalComponent implements OnInit, OnDestroy {
         next: () => {
           this.errorMessage = '';
           this.isLoading = false;
-          this.router.navigateByUrl('settings/payment');
+          this.router.navigateByUrl('settings/payment', { state: { addedPayment: true } });
         },
         error: (err) => {
           this.isLoading = false;
--- frontend/src/app/pages/payment/payment.component.ts
+++ frontend/src/app/pages/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { BackButtonComponent } from 'src/app/components/back-button/back-button.component';
@@ -15,7 +15,7 @@ import { ToastComponent } from 'src/app/components/toast/toast.component';
     styleUrl: './payment.component.css',
     imports: [UserInputComponent, ButtonComponent, RouterLink, BackButtonComponent, ReactiveFormsModule, PaymentOptionsComponent, ToastComponent]
 })
-export class PaymentComponent implements AfterViewInit {
+export class PaymentComponent implements AfterViewInit, OnInit {
   /* Get references to the toast component */
   @ViewChild('toastComponent') toastComponent!: ToastComponent;
 
@@ -24,14 +24,18 @@ export class PaymentComponent implements AfterViewInit {
   public toastMessage = 'Zahlungsmethode hinzugefügt!';
   public toastType: 'success' | 'error' = 'success';
 
-  constructor(private router: Router) {
-    const routerState = this.router.getCurrentNavigation()?.extras?.state;
-    if(routerState && routerState['addedPayment']) {
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    /* Check if a payment method was added and show the toast */
+    if(history.state.addedPayment) {
       this.showToast = true;
     }
+    history.replaceState({}, '');   // Clear the router state to prevent the toast from showing again
   }
 
   ngAfterViewInit(): void {
+    /* Show the toast after the view has been initialized */
     if(this.showToast) {
       this.toastComponent.showToast();
       this.showToast = false;
