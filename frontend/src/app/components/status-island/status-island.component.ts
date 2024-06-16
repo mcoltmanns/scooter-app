6e4b2a029d78d3e4c8548ad42fafc2bf6a15fc4b
Silvan Ronge <silvan.ronge@uni-konstanz.de>
2024-06-16 16:55:17 +0200

    Finalize stauts-island styling

--- frontend/src/app/components/status-island/status-island.component.ts
+++ frontend/src/app/components/status-island/status-island.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, Input, AfterViewInit, OnDestroy, HostListener, AfterViewChecked } from '@angular/core';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Router } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-status-island',
@@ -27,10 +29,16 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
     ])
   ]
 })
-export class StatusIslandComponent implements AfterViewInit, OnDestroy {
+export class StatusIslandComponent implements AfterViewInit, OnDestroy, AfterViewChecked {
   public onDOM = false;
   public isVisible = false;
-  @Input() public showDuration = 70000;
+  @Input() public showCountdown = true;
+  @Input() public showDuration = 600000;
+  @Input() public imgPath: string | null = null;
+  @Input() public redirectPath: string | null = null;
+  @Input() public title: string | null = null;
+  private titleMinLength = 6;
+  public showContent = false;
   private animationDuration = 300;  // Match this with your animation duration, e.g. 0.3s = 300ms
 
   public remainingDuration: number = this.showDuration;
@@ -40,8 +48,15 @@ export class StatusIslandComponent implements AfterViewInit, OnDestroy {
   private showTimeout: ReturnType<typeof setTimeout> | null = null;
   private hideTimeout: ReturnType<typeof setTimeout> | null = null;
 
+  constructor(private router: Router, private cdRef: ChangeDetectorRef) { }
+
   ngAfterViewInit(): void {
+    this.adjustElementsToWindow();
     this.showAndHide();
+  }
+
+  ngAfterViewChecked(): void {
+    this.cdRef.detectChanges();
   }
 
   ngOnDestroy(): void {
@@ -102,6 +117,35 @@ export class StatusIslandComponent implements AfterViewInit, OnDestroy {
     }, this.showDuration);
   }
 
+  @HostListener('window:resize')
+  onWindowResize(): void {
+    this.adjustElementsToWindow();
+  }
+
+  adjustElementsToWindow(): void {
+    this.showContent = true;
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 550) {
+      this.titleMinLength = 50;
+    } else if (screenWidth > 400) {
+      this.titleMinLength = 30;
+    } else if (screenWidth > 320) {
+      this.titleMinLength = 20;
+    } else if (screenWidth > 240) {
+      this.titleMinLength = 8;
+    } else {
+      this.titleMinLength = 6;
+      this.showContent = false;
+    }
+  }
+
+  getShortenedTitle(): string {
+    if (!this.title) {
+      return '';
+    }
+    return this.title.length > this.titleMinLength ? `${this.title.slice(0, this.titleMinLength)}...` : this.title;
+  }
+
   getFormattedTime(): string {
     /* Calculate the number of full minutes in the remaining duration */
     const minutes = Math.floor(this.remainingDuration / 60);
@@ -116,5 +160,16 @@ export class StatusIslandComponent implements AfterViewInit, OnDestroy {
   pad(num: number): string {
     /* If the number is less than 10, prepend a '0'. Otherwise, return the number as a string. */
     return num < 10 ? `0${num}` : num.toString();
-}
+  }
+
+  onIslandClick(): void {
+    if (this.redirectPath) {
+      /* If a path is provided, navigate to it and
+       pass the originState object to the next route if it exists. */
+      const originState = history.state.originState ? { originState: history.state.originState } : {};
+      this.router.navigate([this.redirectPath], { 
+        state: originState
+      });
+    }
+  }
 }
