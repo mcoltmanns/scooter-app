cdfe76f355d7eded272fcac3b2f51f5d1bde1232
maximilian.jaegr <maximilian.jaeger@uni-konstanz.de>
2024-06-26 17:28:02 +0200

    change User Location Folder name

--- frontend/src/app/utils/getUserposition.ts
+++ frontend/src/app/utils/getUserposition.ts
@@ -2,12 +2,12 @@ import { PositionService } from './position.service';
 
 export class GetUserPosition {
     /**
-     * get current latitude and longitude from the user position 
+     * get current latitude and longitude from the user position
      * @param positionService 
      */
     static setUserPosition(positionService: PositionService): void {
         if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(
+            navigator.geolocation.getCurrentPosition( // get current position
               (position) => {
                 positionService.setLatitude(position.coords.latitude); // update the global variable user latitude 
                 positionService.setLongitude(position.coords.longitude); // update the global variable user longitude
